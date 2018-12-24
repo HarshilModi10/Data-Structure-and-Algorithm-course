0d332a68bc274106e459557512e78ddadb7b0162
Harshil Modi <44011636+HarshilModi10@users.noreply.github.com>
2018-12-24 11:48:25 -0500

    Add files via upload

--- BST.py
+++ BST.py
@@ -37,7 +37,63 @@ class binary_search_tree(object):
                 root.left_child = new_node
             else:
                 self.insert_node(data, root.left_child)
+                
+    def remove(self, data):
+        if self.root:
+            self.root = self.remove_node(data, self.root)
+      
+    # Remove node    
+    def remove_node(self, data, node):
+        
+        if node is None:
+            return None
+        
+        if node.key > data:
+            node.left_child = self.remove_node(data, node.left_child)
+            
+        elif node.key < data:
+            node.right_child = self.remove_node(data, node.right_child)
+            
+        else:
+            # If leaf node
+            if not node.right_child and not node.left_child:
+                print("Removing leaf node")
+                del node
+                return None
+            
+            # if no right child
+            elif not node.right_child:
+                print("Removing root node with no right child")
+                temp = node.left_child
+                del node
+                return temp
+            
+            # if not left child
+            elif not node.left_child:
+                print("Removing root node with no left child")
+                temp = node.right_child
+                del node
+                return temp
+            
+            # if contains left and right child 
+            else: 
+                print("Removing root node")
+                pred = self.get_pred(node.left_child)
+                node.key = pred.key
+                node.left_child = self.remove_node(pred.key, node.left_child)
+                
+        return node
     
+    
+    # Get the predecessor of the node to delete
+    def get_pred(self, node):
+        
+        if node.right_child:
+            return self.get_pred(node.right_child)
+        
+        return node
+            
+   
     # helper get_min            
     def get_min_helper(self):      
         if self.root is None:
@@ -88,9 +144,13 @@ def main():
     bst = binary_search_tree()
     bst.insert(5)
     bst.insert(8)
-    bst.insert(3)
+    bst.insert(6)
+    bst.insert(7)
+    bst.insert(10)
+    bst.insert(11)
     bst.insert(13)
-    bst.treverse() # Output: 3, 5, 8, 13
+    bst.remove(8)
+    bst.treverse() # Output: 5, 6, 7, 10, 11, 13
 
 main()
             
